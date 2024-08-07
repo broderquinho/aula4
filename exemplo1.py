c53a1a798d7b403a7c75e7e30bc8740a1eb88695
broderquinho <broderquinho@gmail.com>
2024-08-06 21:23:41 -0300

    Add files via upload

---
+++ exemplo1.py
@@ -0,0 +1,27 @@
+#Mu Editor - Pygame Zero mode
+import pgzrun
+
+alien = Actor('alien')
+alien.topright = 0, 10
+
+WIDTH = 500
+HEIGHT = alien.height + 20
+
+def draw():
+    screen.clear()
+    alien.draw()
+
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        sounds.eep.play()
+        alien.image = 'alien_hurt'
+    else:
+        alien.image = 'alien'
+
+pgzrun.go()
